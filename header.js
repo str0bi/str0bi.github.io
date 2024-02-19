135c1599e2d62aa087d5ef257a346edf72e7da14
Miu <splasherhaard@outlook.com>
2024-02-19 22:04:24 +0000

    JS Header V1

---
+++ header.js
@@ -0,0 +1,5 @@
+document.getElementById('headerContainer').innerHTML = `
+<p id="subtext"><br /> <br /> <br />  
+        Color palette from <a  id="subtext" href="https://github.com/catppuccin/catppuccin">Catppuccin</a> (Macchiato)<br />
+        2024 Sofie Jóns. <a id="subtext" href="https://github.com/vqia/vqia.github.io">Source Code</a></p>
+`
