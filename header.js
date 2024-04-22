f9ed648d448391f48327838e234f014689b725af
Miu <splasherhaard@outlook.com>
2024-04-22 18:34:53 +0000

    new entries, new bio, new name

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 document.getElementById('headerContainer').innerHTML = `
 <p id="subtext"><br /> <br /> <br />  
-        Color palette from <a  id="subtext" href="https://github.com/catppuccin/catppuccin">Catppuccin</a> (Macchiato)<br />
-        2024 Sofie Jóns. <a id="subtext" href="https://github.com/vqia/vqia.github.io">Source Code</a></p>
+        Color palette from <a  id="subtext" href="https://github.com/catppuccin/catppuccin">Catppuccin</a> (Macchiato).<br />
+        2024 Max Jónsbur. <a id="subtext" href="https://github.com/vqia/vqia.github.io">Source Code</a></p>
 `
